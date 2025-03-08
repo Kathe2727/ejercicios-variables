52442ce219c19be66bf75ac7102feb58234c172a
Katherine <vargaskatherinne27@gmail.com>
2025-03-08 00:09:47 -0500

    Agregado cálculo del perímetro de un círculo

---
+++ js/ejercicio5.js
@@ -0,0 +1,6 @@
+const numero1 = parseFloat(prompt("Ingresa el primer número:"));
+const numero2 = parseFloat(prompt("Ingresa el segundo número:"));
+const numero3 = parseFloat(prompt("Ingresa el tercer número:"));
+
+const promedio = (numero1 + numero2 + numero3) / 3;
+console.log(`El promedio de los números ${numero1}, ${numero2} y ${numero3} es: ${promedio}`);
---
+++ js/ejercicio6.js
@@ -0,0 +1,5 @@
+const radio = parseFloat(prompt("Ingresa el radio del círculo:"));
+
+const perimetro = 2 * Math.PI * radio;
+
+console.log(`El perímetro del círculo con radio ${radio} es: ${perimetro}`);
